a63623d94453aa78222a3e16baec412eba09dae0
Julio Montaño <jmz7v.com@gmail.com>
2017-09-20 11:53:08 -0500

    Donations as object

--- src/donations/index.js
+++ src/donations/index.js
@@ -1,84 +1,66 @@
 import React, { PropTypes } from 'react'
 import classNames from 'classnames'
 
+const SOURCE = [
+  {
+    link: 'http://www.topos.mx/',
+    label: 'Topos – Brigada de Rescate Topos Tlaltelolco, A.C. (Earthquake help team)'
+  },
+  {
+    link: 'https://www.amazon.com.mx/b?ie=UTF8&node=17290014011',
+    label: 'Cruz Rojo Mexico (Red Cross Mexico) - Amazon Wishlist '
+  },
+  {
+    link: 'https://www.donaunicef.org.mx/landing-terremoto/?utm_source=mpr_redes&utm_campaign=tw-terremoto&utm_medium=tw&utm_content=tw-org&utm_term=tw-org',
+    label: 'UNICEF Mexico '
+  },
+  {
+    link: 'https://www.globalgiving.org/projects/mexico-earthquake-and-hurricane-relief-fund/',
+    label: 'Mexico Earthquake Relief Fund'
+  },
+  {
+    link: 'http://www.elfinanciero.com.mx/nacional/quieres-ayudar-aqui-se-instalaran-los-centros-de-acopio.html',
+    label: 'Centros de Acopio en DF/Local Support Center in Mexico City '
+  },
+  {
+    link: 'http://www.projectpaz.org/',
+    label: 'Support Mexico Earthquake Relief by Project Paz (New York based non-profit)'
+  },
+  {
+    link: 'https://www.youcaring.com/mexicorecoveryeffort-955816',
+    label: 'Unión del Barrio (CA) México Recovery Effort Donation Page'
+  },
+  {
+    link: 'http://comoayudar.mx/',
+    label: 'Collection of different donation options/Collecion de varias opcionés para donar a Mx'
+  },
+  {
+    link: 'https://www.gofundme.com/b7uj4b-supporting-mexico',
+    label: 'Supporting Mexico'
+  },
+]
+
 const Donations = props => {
+  let donations = SOURCE.map(function (don, index) {
+    return (
+      <li
+        key={index}
+      >
+        <a
+          target='_blank'
+          href={don.link}
+        >
+          {don.label}
+        </a>
+      </li>
+    )
+  }.bind(this))
   return (
     <div className='section'>
       <div className='content'>
         <h2 className='title'>Donaciones</h2>
         <ul>
-          <li>
-            <a
-              href='http://www.topos.mx/'
-              target='_blank'
-            >
-              Topos – Brigada de Rescate Topos Tlaltelolco, A.C. (Earthquake help team)
-            </a>
-          </li>
-          <li>
-            <a
-              href='https://www.amazon.com.mx/b?ie=UTF8&node=17290014011'
-              target='_blank'
-            >
-              Cruz Rojo Mexico (Red Cross Mexico) - Amazon Wishlist 
-            </a>
-          </li>
-          <li>
-            <a
-              href='https://www.donaunicef.org.mx/landing-terremoto/?utm_source=mpr_redes&utm_campaign=tw-terremoto&utm_medium=tw&utm_content=tw-org&utm_term=tw-org'
-              target='_blank'
-            >
-              UNICEF Mexico 
-            </a>
-          </li>
-          <li>
-            <a
-              href='https://www.globalgiving.org/projects/mexico-earthquake-and-hurricane-relief-fund/'
-              target='_blank'
-            >
-              Mexico Earthquake Relief Fund
-            </a>
-          </li>
-          <li>
-            <a
-              href='http://www.elfinanciero.com.mx/nacional/quieres-ayudar-aqui-se-instalaran-los-centros-de-acopio.html'
-              target='_blank'
-            >
-              Centros de Acopio en DF/Local Support Center in Mexico City 
-            </a>
-          </li>
-          <li>
-            <a
-              href='http://www.projectpaz.org/'
-              target='_blank'
-            >
-              Support Mexico Earthquake Relief by Project Paz (New York based non-profit)
-            </a>
-          </li>
-          <li>
-            <a
-              href='https://www.youcaring.com/mexicorecoveryeffort-955816'
-              target='_blank'
-            >
-              Unión del Barrio (CA) México Recovery Effort Donation Page
-            </a>
-          </li>
-          <li>
-            <a
-              href='http://comoayudar.mx/'
-              target='_blank'
-            >
-              Collection of different donation options/Collecion de varias opcionés para donar a Mx
-            </a>
-          </li>
-          <li>
-            <a
-              href='https://www.gofundme.com/b7uj4b-supporting-mexico'
-              target='_blank'
-            >
-              Supporting Mexico
-            </a>
-          </li>
+          {donations}
         </ul>
       </div>
     </div>
