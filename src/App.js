da32ffbf9f0abc6e60952325f610a738157a2fc8
Diego Lara <dyegolara@gmail.com>
2017-09-20 00:49:38 -0500

    Tabs ready

--- src/App.js
+++ src/App.js
@@ -1,31 +1,90 @@
 import React, { Component } from 'react'
+import { browserHistory } from 'react-router'
+import classNames from 'classnames'
 import './bulma.css'
 
+import People from './people'
+import Buildings from './buildings'
+import Shelters from './shelters'
+
+const TABS = [
+  {
+    label: 'Personas',
+    slug: '/personas'
+  },
+  {
+    label: 'Edificios',
+    slug: '/edificios'
+  },
+  {
+    label: 'Albergues',
+    slug: '/albergues'
+  }
+]
+
 class App extends Component {
+  constructor (props) {
+    super(props)
+    console.log(props)
+    let path = props.location.pathname
+    let test = TABS.filter((tab) => { if (path.includes(tab.slug)) return tab })
+    this.data = {
+      currentTab: (typeof test[0] !== 'undefined') ? test[0].label : TABS[0].label
+    }
+    this.state = Object.assign({}, this.data)
+  }
+
+  // Set current tab to tab
+  setTab (tab) {
+    this.setState({'currentTab': TABS[tab].label})
+    browserHistory.push(`${TABS[tab].slug}`)
+  }
+
+  renderTabs () {
+    let renderTabs = TABS.map((tab, index) => {
+      return (
+        <li
+          key={index}
+          className={classNames({'is-active': tab.label === this.state.currentTab})}
+          onClick={this.setTab.bind(this, index)}
+        >
+          <a>{tab.label}</a>
+        </li>
+      )
+    })
+    return (
+      <div className='tabs is-centered is-boxed is-medium'>
+        <ul>
+          {renderTabs}
+        </ul>
+      </div>
+    )
+  }
+
+  renderContent () {
+    let content = null
+
+    switch (this.state.currentTab) {
+      case TABS[0].label:
+        content = (<People {...this.props} />)
+        break
+      case TABS[1].label:
+        content = (<Buildings {...this.props} />)
+        break
+      case TABS[2].label:
+        content = (<Shelters{...this.props} />)
+        break
+    }
+
+    return content
+  }
 
   render () {
     return (
       <div className='container'>
         <h1 className='title'>Encontrar gente sismo</h1>
-        <div className='tabs is-centered is-boxed is-medium'>
-          <ul>
-            <li className='is-active'>
-              <a>
-                <span>Personas</span>
-              </a>
-            </li>
-            <li>
-              <a>
-                <span>Edificios</span>
-              </a>
-            </li>
-            <li>
-              <a>
-                <span>Albergues</span>
-              </a>
-            </li>
-          </ul>
-        </div>
+        {this.renderTabs()}
+        {this.renderContent()}
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Router, Route, Redirect, browserHistory } from 'react-router'
 import './index.css'
 import App from './App'
+
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const $app = document.getElementById('root')
+
+const routes = (
+  <Route component={App}>
+    <Route path='/personas' component={App} />
+    <Route path='/edificios' component={App} />
+    <Route path='/albergues' component={App} />
+    <Redirect from='/' to='/personas' />
+  </Route>
+)
+
+ReactDOM.render(<Router routes={routes} history={browserHistory} />, $app)
 registerServiceWorker()
