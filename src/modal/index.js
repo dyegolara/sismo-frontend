7c3af5fc4d892613885ce1fd866a362e9c947c4b
Diego Lara <dyegolara@gmail.com>
2017-09-20 02:30:08 -0500

    Add Modal component

---
+++ src/modal/index.js
@@ -0,0 +1,56 @@
+import React, { PropTypes } from 'react'
+import classNames from 'classnames'
+
+import Button from 'shared/button'
+
+const Modal = props => {
+  return (
+    <div className={classNames('modal', {'is-active': props.isActive})}>
+      <div className='modal-background' onClick={props.toggleModal} />
+      <div className='modal-card'>
+        <header className='modal-card-head'>
+          <p className='modal-card-title'>{props.title}</p>
+          <button
+            className='delete'
+            onClick={props.toggleModal}
+          />
+        </header>
+        <section className='modal-card-body'>
+          {props.children}
+        </section>
+        <footer className='modal-card-foot columns'>
+          <div className='control column is-3'>
+            <Button
+              buttonStyle='dangerInv'
+              className='is-fullwidth'
+              onClick={props.toggleModal}
+            >Cancelar</Button>
+          </div>
+          <div className='control column is-3 is-offset-6'>
+            <Button
+              buttonStyle='primary'
+              className='is-fullwidth'
+              onClick={props.onSubmit}
+              icon='check'
+            >Aceptar</Button>
+          </div>
+        </footer>
+      </div>
+    </div>
+  )
+}
+
+export default Modal
+
+Modal.propTypes = {
+  isActive: PropTypes.bool.isRequired,
+  toggleModal: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  title: PropTypes.string
+}
+
+Modal.defaultProps = {
+  isActive: false,
+  onSubmit: () => {},
+  title: ''
+}
