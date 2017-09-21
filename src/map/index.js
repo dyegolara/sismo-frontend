e2c3b514227f86330e6a8f9cf268dd20184b7b88
Diego Lara <dyegolara@gmail.com>
2017-09-21 11:06:58 -0500

    Fix map link

--- src/map/index.js
+++ src/map/index.js
@@ -1,20 +1,19 @@
-import React, { PropTypes } from 'react'
-import classNames from 'classnames'
+import React  from 'react'
 
-const mapLink = 'https://www.google.com/maps/d/viewer?mid=1PwJrCIjz5PNfKAFrY-EX-iEkWH8'
+const mapLink = 'http://google.org/crisismap/google.com/puebla-mexico-earthquake-es'
 const mapEmbed = 'https://www.google.com/maps/d/embed?mid=1PwJrCIjz5PNfKAFrY-EX-iEkWH8'
 
 const Map = props => {
   return (
     <div className='section'>
       <div className='content'>
-        <p>Link directo al mapa <a href='{mapLink}' target='_blank'>{mapLink}</a></p>
+        <p>Link directo al mapa <a href={mapLink} target='_blank'>{mapLink}</a></p>
         <iframe
           src={mapEmbed}
           width='100%'
           height='500'
-        ></iframe>
-        <p>Link directo al mapa <a href='{mapLink}' target='_blank'>{mapLink}</a></p>
+        />
+        <p>Link directo al mapa <a href={mapLink} target='_blank'>{mapLink}</a></p>
       </div>
     </div>
   )
