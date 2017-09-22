61647529c224bf3671457c946e376d454af4c988
Diego Lara <dyegolara@gmail.com>
2017-09-21 20:59:31 -0500

    fix twitter buzany

--- src/footer/index.js
+++ src/footer/index.js
@@ -6,7 +6,7 @@ const Footer = props => {
     <div className='section'>
       <div className='content'>
         <p className='has-text-centered'>
-          Hackeado por <a href='https://twitter.com/jmz7v' target='_blank'>Julio</a>, <a href='https://twitter.com/dyegolara' target='_blank'>Diego</a>, <a href='https://twitter.com/cesarslh' target='_blank'>Angel</a> y <a href='https://twitter.com/LuisOsnet' target='_blank'>Luis</a> .
+          Hackeado por <a href='https://twitter.com/jmz7v' target='_blank'>Julio</a>, <a href='https://twitter.com/dyegolara' target='_blank'>Diego</a>, <a href='https://twitter.com/abuzzany' target='_blank'>Angel</a> y <a href='https://twitter.com/LuisOsnet' target='_blank'>Luis</a> .
         </p>
       </div>
     </div>
