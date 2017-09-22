15f22e7d04026d306587a9c85c220efe7992e302
Diego Lara <dyegolara@gmail.com>
2017-09-21 21:25:36 -0500

    Reconvert shelters to dynamic

--- src/shelters.js
+++ src/shelters.js
@@ -1,104 +1,217 @@
-import React from 'react'
+import React, { Component } from 'react'
+import _pickBy from 'lodash/pickBy'
 
-export default () => {
-  return (
-    <div className='content'>
-      <p className='new-element-container'><em>* Actualizado 20 de septiembre, 8:00&nbsp;</em></p>
-      <h3 className='subtitle'>Delegación Álvaro Obregón</h3>
-      <ul>
-        <li>Deportivo G3. Av. Escuadrón 201 s/n Col. Victoria.</li>
-        <li>Salón de usos múltiples, Calle 10 y Canarios, Col. Tolteca.</li>
-      </ul>
-      <h3 className='subtitle'>Delegación Azcapotzalco</h3>
-      <ul>
-        <li>CAIS Azcapotzalco. Av del Rosario s/n, Col. san Martín, Xochinahuac.</li>
-      </ul>
-      <h3 className='subtitle'>Delegación Benito Juárez</h3>
-      <ul>
-        <li>Deportivo Benito Juárez. Calle municipio libre y Calle Uxmal 807-B, Sta. Cruz Atoyac, 03310 Ciudad de México, CDMX</li>
-        <li>Cumbres de Acutzingo y Eje 4 Xola.</li>
-      </ul>
-      <h3 className='subtitle'>Delegación Coyoacán</h3>
-      <ul>
-        <li>Deportivo Rosario Iglesias. Calz. del Hueso 400, Col. Los Girasoles, 04920&nbsp;</li>
-        <li>Deportivo Culhuacanes. Calzada Taxqueña 1811, Col. Ex-ejido de San Francisco, Culhuacán, Del. Coyoacán</li>
-        <li>Gimnasio Coyoacán, calle Cuauhtémoc s/n, esquina Aguayo, Col. El Carmen</li>
-        <li>Gimnasio Ajusco. Coras s/n, Col. Ajusco, Del. Coayoacán</li>
-        <li>Centro de Desarrollo Comunitario. Calzada de las bombas s/n casi esquina con Cafetales.</li>
-        <li>Casa de las Letras, Canal de Miramontes y Av. del Parque.</li>
-        <li>Dirección territorial Culhuacenes. Rosa María Sequeira y La Virgen.</li>
-        <li>Deportivo de los Electricistas Club deportivo girasoles. Calzada del Hueso 381.</li>
-      </ul>
-      <h3 className='subtitle'>Delegación Cuauhtémoc</h3>
-      <ul>
-        <li>Deportivo Cuauhtémoc. Aldama y Jesús García, Col. Buenavista</li>
-        <li>Club Junior. Baja California esquina Nuevo León, Col. Hipódromo</li>
-        <li>Deportivo Mina, calle Mina entre Héroes y Soto, Col. Guerrero</li>
-        <li>Preparatoria Fresno, entre Fresno y Sabino, Col. Atlampa</li>
-        <li>Hogar CDMX. Calle Bernal Díaz del Castillo número 34, col. Buenavista</li>
-        <li>Salón de usos múltiples. Chimalpopoca 273, col. Tránsito.</li>
-      </ul>
-      <h3 className='subtitle'>Delegación Cuaujimalpa</h3>
-      <ul>
-        <li>Deportivo Morelos. José María Castorena s/n, esquina san José de los Cedros</li>
-      </ul>
-      <h3 className='subtitle'>Delegación Gustavo A. Madero</h3>
-      <ul>
-        <li>Casa del peregrino. Eje 5 Nte Calz San Juan de Aragón 63, Villa Gustavo A. Madero, 07050 Ciudad de México, CDMX</li>
-        <li>CAIS Villa Mujeres. Margarita Maza de Juárez 150 Bis, La Patera Vallejo, 07710 Ciudad de México, CDMX</li>
-        <li>Calle Sierra Vista esquina Cayan, colonia Lindavista</li>
-      </ul>
-      <h3 className='subtitle'>Delegación Iztacalco</h3>
-      <ul>
-        <li>Sala de Armas de la Magdalena Mixihuca. Ciudad Deportiva de la Magdalena Mixhuca, puerta 6, col. Granjas México</li>
-        <li>Casa del Adulto Mayor Elena Poniatowska. Calle 1 s/n, entre Guadalupe y talleres gráficos, Colonia Pantitlán.</li>
-        <li>Centro Social Fraccionamiento Coyuya. Av Coyuya s/n, esquina 1ero de mayo, Colonia Fraccionamiento Coyuya.</li>
-        <li>Casa de la Mujer Mariposas Iztacalco. Sur 20 y Oriente 227, Col Agrícola Oriental</li>
-        <li>CAIS Estudiante y albergue Coruña. Calle Sur 65-A, número 3246, Col. Viaducto Piedad</li>
-      </ul>
-      <h3 className='subtitle'>Delegación Iztapalapa</h3>
-      <ul>
-        <li>Centro Comunitario Luis Donaldo Colosio. Lázaro Cárdenas s/n entre 20 de noviembre y Jalisco, col. Ixtlahuacán</li>
-        <li>Centro Villa Estrella. Camino al Cerro de la Estrella s/n, col. El Santuario.</li>
-        <li>Albergue habilitado Centro Vientos de Libertad. Calle súper manzana 1, manzana 18, Col. Ejército.</li>
-        <li>Centro Violeta Parra. Lateral de Churubusco s/n sobre camellón, Col. San José Aculco.</li>
-        <li>Coruña Jóvenes. Sur 65-A 3246, Viaducto Piedad</li>
-        <li>Stand de tiro PGJCDMX. Cabeza de Juárez</li>
-        <li>Centro Cívico Lomas Estrella en Macedonia</li>
-      </ul>
-      <h3 className='subtitle'>Delegación Magdalena Contreras</h3>
-      <ul>
-        <li>Calle Atacaxco&nbsp;</li>
-        <li>Calle Cazulco</li>
-        <li>Casa Popular. Av. Luis Cabrera esq. Av. Contreras, col. San Jerónimo Lídice.</li>
-      </ul>
-      <h3 className='subtitle'>Delegación Miguel Hidalgo</h3>
-      <ul>
-        <li>Explanada de la Delegación, Parque Lira y Vicente Eguía.</li>
-        <li>Deportivo Plan Sexenal. Calle Plan de Guadalupe y Ferrocarril de Cuernavaca s/n, Miguel Hidalgo, Nextitla, 11420 Ciudad de México, CDMX</li>
-        <li>Deportivo José María Morelos y Pavón. Lago Trasimeno s/n, Pensil Norte, Reforma Pensil.</li>
-        <li>Parque Lira. Calle Parque Lira 136, San Miguel Chapultepec I Secc, 11850 Ciudad de México</li>
-        <li>Calle Benjamín Franklin, col. Daniel Garza (Metro Tacubaya).</li>
-        <li>Centro Modelo, Grupo Modelo. Cerrada Palomas 22. Col. Reforma Social</li>
-      </ul>
-      <h3 className='subtitle'>Delegación Tláhuac</h3>
-      <ul>
-        <li>Centro de fe. Carretera a Santa Catarina y Montes Carpatos, col Tlaltenco.</li>
-      </ul>
-      <h3 className='subtitle'>Delegación Tlalpan</h3>
-      <ul>
-        <li>Deportivo La Joya. Av. Insurgentes Sur 4439, Col. La Joya.</li>
-        <li>Deportivo Villa Olímpica. Avenida Insurgentes Sur 3493, Tlalpan, Co. Parques del Pedregal.</li>
-      </ul>
-      <h3 className='subtitle'>Delegación Milpa Alta</h3>
-      <ul>
-        <li>Gimnasio Villa Milpa Alta. Domicilio conocido.</li>
-      </ul>
-      <h3 className='subtitle'>Delegación Xochimilco</h3>
-      <ul>
-        <li>Pista Olímpica de remo y canotaje Virgilio Uribe. Antiguo canal de Cuernavaca 4557</li>
-      </ul>
-      <p><em>Se actualizará constantemente esta información</em></p>
-    </div>
-  )
+import API from './api'
+import TextField from './textField'
+import Modal from './modal'
+import Button from './button'
+
+export default class Shelters extends Component {
+  constructor () {
+    super()
+    this.data = {
+      shelters: [],
+      reqInProg: false,
+      nombre: '',
+      direccion: '',
+      espacios: 0,
+      necesidades: '',
+      id: ''
+    }
+    this.state = Object.assign({}, this.data)
+  }
+
+  componentDidMount () {
+    this.loadData()
+  }
+
+  onChangeAddress (e) {
+    this.setState({direccion: e.target.value})
+  }
+
+  onChangeName (e) {
+    this.setState({nombre: e.target.value})
+  }
+
+  onChangeSpaces (e) {
+    this.setState({espacios: e.target.value})
+  }
+
+  onChangeNeeds (e) {
+    this.setState({necesidades: e.target.value})
+  }
+
+  toggleModal () {
+    this.setState({modalOpen: !this.state.modalOpen}, this.resetState)
+  }
+
+  resetState () {
+    this.setState({
+      direccion: this.data.direccion,
+      nombre: this.data.nombre,
+      espacios: this.data.espacios,
+      necesidades: this.data.necesidades,
+      id: this.data.id
+    })
+  }
+
+  setShelter (shelter) {
+    this.setState({
+      modalOpen: !this.state.modalOpen,
+      direccion: shelter.direccion,
+      nombre: shelter.nombre,
+      espacios: shelter.espacios,
+      necesidades: shelter.necesidades,
+      id: shelter.id
+    })
+  }
+
+  onSubmit (id = '') {
+    let { direccion, nombre, espacios, necesidades, shelters } = this.state
+    let data = {
+      direccion,
+      nombre,
+      espacios,
+      necesidades
+    }
+    if (id) {
+      API.Shelters.Update(id, data)
+        .then(response => {
+          let updatedShelter = response.albergue
+          shelters = shelters.map(shelter => shelter.id === updatedShelter.id ? updatedShelter : shelter)
+          this.setState({shelters}, this.resetState)
+        })
+    }
+    API.Shelters.SendNewds(data)
+      .then(response => {
+        shelters.push(response.albergue)
+        this.setState({shelters})
+      })
+    this.toggleModal()
+  }
+
+  // Get clients
+  loadData () {
+    let {filters} = this.state
+    let _filters = _pickBy(filters, filter => filter.length > 0)
+    let params = Object.assign({}, _filters)
+    this.setState({reqInProg: true})
+    API.Shelters.GetList(params)
+      .then(response => {
+        this.setState({shelters: response.albergues, reqInProg: false})
+      })
+  }
+
+  renderNewButton () {
+    return (
+      <div className='new-element-container'>
+        <Button
+          className='is-pulled-right'
+          buttonStyle='primary'
+          onClick={this.toggleModal.bind(this)}
+          icon='plus'
+        >
+          Nuevo Reporte de Albergue
+        </Button>
+      </div>
+    )
+  }
+
+  renderTable () {
+    let {shelters, reqInProg} = this.state
+    if (reqInProg) return <div>Cargando...</div>
+    let table = <div>No hay resultados...</div>
+    if (shelters.length > 0) {
+      let sheltersList = shelters.map((shelter, index) => {
+        return (
+          <tr
+            key={`shelters-${index}`}
+            onClick={this.setShelter.bind(this, shelter)}
+          >
+            <td>{shelter.direccion}</td>
+            <td>{shelter.nombre}</td>
+            <td>{shelter.espacios}</td>
+            <td>{shelter.necesidades}</td>
+          </tr>
+        )
+      })
+      table = (
+        <div>
+          <table className='table is-striped is-fullwidth'>
+            <thead>
+              <tr>
+                <th>Dirección</th>
+                <th>Contacto</th>
+                <th>Espacios disponibles</th>
+                <th>Necesidades</th>
+              </tr>
+            </thead>
+            <tbody>
+              {sheltersList}
+            </tbody>
+          </table>
+        </div>
+      )
+    }
+    return table
+  }
+
+  renderModal () {
+    return (
+      <Modal
+        title='Nuevo Reporte de Albergue'
+        isActive={this.state.modalOpen}
+        toggleModal={this.toggleModal.bind(this)}
+        onSubmit={this.onSubmit.bind(this, this.state.id)}
+      >
+        <div>
+          <div className='columns'>
+            <div className='column'>
+              <TextField
+                label='Dirección'
+                value={this.state.direccion}
+                onChange={this.onChangeAddress.bind(this)}
+              />
+            </div>
+          </div>
+          <div className='columns'>
+            <div className='column'>
+              <TextField
+                label='Contacto'
+                value={this.state.nombre}
+                onChange={this.onChangeName.bind(this)}
+              />
+            </div>
+            <div className='column'>
+              <TextField
+                label='Cupo'
+                value={this.state.espacios}
+                onChange={this.onChangeSpaces.bind(this)}
+                type='number'
+              />
+            </div>
+          </div>
+          <div className='columns'>
+            <div className='column'>
+              <TextField
+                label='Necesidades'
+                value={this.state.necesidades}
+                onChange={this.onChangeNeeds.bind(this)}
+              />
+            </div>
+          </div>
+        </div>
+      </Modal>
+    )
+  }
+
+  render () {
+    return (
+      <div>
+        {this.renderNewButton()}
+        {this.renderTable()}
+        {this.renderModal()}
+      </div>
+    )
+  }
 }
